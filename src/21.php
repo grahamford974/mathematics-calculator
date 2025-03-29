e487a21f8d3b82866f662f45675acf7b06677c3e
grahamford974 <grahamford974@gmail.com>
2025-03-29 18:58:39 +0000

    Add 21.php

---
+++ src/21.php
@@ -0,0 +1,15 @@
+<?php
+// Define variables
+$operation = 'add'; // Add or subtract
+
+// Input values
+$value1 = 5;
+$value2 = 3;
+
+// Perform calculation
+$result = $value1 + $value2; // $result will hold the sum of value1 and value2
+
+// Output result
+echo "The result is: " . $result;
+
+?>
