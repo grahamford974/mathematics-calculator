6b5d38cafa51e86b25f91db3bbccb7ac2727eb98
grahamford974 <grahamford974@gmail.com>
2025-04-05 05:47:11 +0000

    Add 26.php

---
+++ src/26.php
@@ -0,0 +1,16 @@
+<?php
+// Basic arithmetic operations: addition, subtraction, multiplication, division
+
+$a = 5;
+$b = 3;
+
+$result_addition = $a + $b;
+$result_subtraction = $a - $b;
+$result_multiplication = $a * $b;
+$result_division = $a / $b;
+
+echo "Addition: $result_addition";
+echo "\nSubtraction: $result_subtraction";
+echo "\nMultiplication: $result_multiplication";
+echo "\nDivision: $result_division";
+?>
