6fbcc5ff2de111b4e3315a823f64f15affecceac
grahamford974 <grahamford974@gmail.com>
2025-04-07 14:32:56 +0000

    Add 28.php

---
+++ src/28.php
@@ -0,0 +1,12 @@
+<?php
+// Example PHP script for adding two numbers
+function addNumbers($num1, $num2) {
+    return $num1 + $num2;
+}
+
+$number1 = 5;
+$number2 = 3;
+
+$result = addNumbers($number1, $number2);
+echo "The sum of $number1 and $number2 is: $result";
+?>
