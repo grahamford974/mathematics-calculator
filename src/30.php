5af8d22d75a54b5cc567a9eb0d95d327f9ce8455
grahamford974 <grahamford974@gmail.com>
2025-04-09 19:34:34 +0000

    Add 30.php

---
+++ src/30.php
@@ -0,0 +1,11 @@
+<?php
+// This is an example PHP script for a simple mathematics calculator.
+echo "Enter first number: ";
+$number1 = readline();
+echo "Enter second number: ";
+$number2 = readline();
+
+$result = $number1 + $number2;
+echo "Result: $result";
+
+?>
