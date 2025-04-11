143fb2900ba0657333737eb30709b18e0ed8a7ac
grahamford974 <grahamford974@gmail.com>
2025-04-11 12:35:39 +0000

    Add 37.php

---
+++ src/37.php
@@ -0,0 +1,23 @@
+<?php
+class MathCalculator {
+    public function add($num1, $num2) {
+        return $num1 + $num2;
+    }
+
+    public function subtract($num1, $num2) {
+        return $num1 - $num2;
+    }
+
+    public function multiply($num1, $num2) {
+        return $num1 * $num2;
+    }
+
+    public function divide($num1, $num2) {
+        if ($num2 != 0) {
+            return $num1 / $num2;
+        } else {
+            throw new Exception("Cannot divide by zero");
+        }
+    }
+}
+?>
