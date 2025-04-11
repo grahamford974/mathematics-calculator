0f366f9682f1e5efe563c09faaa665ad30384d2a
grahamford974 <grahamford974@gmail.com>
2025-04-11 13:04:25 +0000

    Add 52.php

---
+++ src/52.php
@@ -0,0 +1,7 @@
+<?php
+echo "Enter first number: ";
+$firstNumber = readline();
+echo "Enter second number: ";
+$secondNumber = readline();
+echo $firstNumber . " + " . $secondNumber . " = " . ($firstNumber + $secondNumber) . "\n";
+?>
