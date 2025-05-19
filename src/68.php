706d35211b9b45d22c311bfca606d8f0cbced051
grahamford974 <grahamford974@gmail.com>
2025-05-19 05:13:56 +0000

    Add 68.php

---
+++ src/68.php
@@ -0,0 +1,9 @@
+<?php
+// Example of a simple PHP function to add two numbers
+function addNumbers($a, $b) {
+    return $a + $b;
+}
+
+// Test the function with two numbers
+echo "The sum of 5 and 3 is: " . addNumbers(5, 3);
+?>
