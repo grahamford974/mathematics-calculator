278984ff25cbef1ce3ef865de031436d0b82e801
grahamford974 <grahamford974@gmail.com>
2025-05-24 08:07:30 +0000

    Add 70.php

---
+++ src/70.php
@@ -0,0 +1,21 @@
+<?php
+
+function add($a, $b) {
+    return $a + $b;
+}
+
+function subtract($a, $b) {
+    return $a - $b;
+}
+
+function multiply($a, $b) {
+    return $a * $b;
+}
+
+function divide($a, $b) {
+    if ($b != 0) {
+        return $a / $b;
+    } else {
+        throw new Exception("Divisor cannot be zero.");
+    }
+}
